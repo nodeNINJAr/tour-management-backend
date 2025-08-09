5765042d7843f26faff9bf7a88c99b2bdcf22be2
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-08-09 20:03:14 +0600

    global err handler and file muduler-added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors"
 import { router } from './routes';
+import { envVars } from './app/confiq/env';
 
 
 // 
@@ -26,4 +27,23 @@ app.get("/", (req:Request, res:Response)=>{
     })
 })
 
+
+// global error handelr
+app.use((err:any, req:Request, res:Response, next:NextFunction)=>{
+    res.status(500).json({
+        success:false,
+        message:`Something went wrong ${err.message} catch on global err handler `,
+        err,
+        // stack used for showing which line error come from 
+        stack:envVars.NODE_ENV === "development" ? err.stack : null
+    })
+})
+
+
+
+
+
+
 export default app
+
+// db --> model --> services --> controller --> routeMatching
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,19 +1,17 @@
-import { Request, Response } from "express";
-import { User } from "./user.model";
+import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes"
+import { UserServices } from "./user.services";
+
+
 
 
 // create user
-const createUser = async(req:Request, res:Response)=>{
+const createUser = async(req:Request, res:Response, next:NextFunction)=>{
      try{
-       //    
-       const {name, email} = req.body;
-       //
-       const user = await User.create({
-         name,
-         email,
-       })    
-
+      
+      // import from user services
+      const user = await UserServices.createUser(req.body)
+      // 
       res.status(httpStatus.CREATED).json({
          message:`User Created Successfully`,
          user
@@ -23,9 +21,7 @@ const createUser = async(req:Request, res:Response)=>{
      // eslint-disable-next-line @typescript-eslint/no-explicit-any
      }catch(err:any){
       console.log(err);
-      res.status(httpStatus.BAD_REQUEST).json({
-        message:`Something went wrong ${err.message}`
-      })
+      next(err)
       
      }
 
