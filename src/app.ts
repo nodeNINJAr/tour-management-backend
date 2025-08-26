fd09bd18f9b6da6eadc5935439b7449adbaad83c
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-08-26 14:51:13 +0600

    passport.js-add-and-env-setup

--- src/app.ts
+++ src/app.ts
@@ -4,20 +4,26 @@ import { router } from './routes';
 import { globalErrorHandler } from './app/middleware/globalErrorHandler';
 import { notFound } from './app/middleware/notFoundHandler';
 import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import expressSession from "express-session";
 
 
 // 
 const app = express();
 
 
-
 // middleware
+app.use(expressSession({
+    secret:"Your secret",
+    resave:false,
+    saveUninitialized:false,
+}
+));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-
-
-
 
 
 // 
--- src/app/confiq/env.ts
+++ src/app/confiq/env.ts
@@ -13,6 +13,11 @@ interface EnvConfig {
     SUPER_ADMIN_PASS:string,
     JWT_REFRESH_SECRET:string,
     JWT_REFRESH_EXPIRES:string,
+    GOOGLE_CLIENT_SECRET:string,
+    GOOGLE_CLIENT_ID:string,
+    GOOGLE_CALLBACK_URL:string,
+    EXPRESS_SESSION_SECRET:string,
+    FRONTEND_URL:string,
 }
 
 
@@ -21,7 +26,8 @@ const loadEnvVars =():EnvConfig => {
 
 // 
 const requiredEnvVariables : string[] = ["PORT", "DB_URL", "NODE_ENV","JWT_ACCESS_SECRET","JWT_ACCESS_EXPIRES"
-  ,"BCRIPT_SOLT_ROUND","SUPER_ADMIN_EMAIL","SUPER_ADMIN_PASS","JWT_REFRESH_SECRET","JWT_REFRESH_EXPIRES"];
+  ,"BCRIPT_SOLT_ROUND","SUPER_ADMIN_EMAIL","SUPER_ADMIN_PASS","JWT_REFRESH_SECRET","JWT_REFRESH_EXPIRES",
+  "GOOGLE_CLIENT_SECRET","GOOGLE_CLIENT_ID","GOOGLE_CALLBACK_URL","EXPRESS_SESSION_SECRET","FRONTEND_URL"];
     // 
     requiredEnvVariables.forEach(key=>{
        if(!process.env[key]){
@@ -42,6 +48,10 @@ const requiredEnvVariables : string[] = ["PORT", "DB_URL", "NODE_ENV","JWT_ACCES
     BCRIPT_SOLT_ROUND:process.env.BCRIPT_SOLT_ROUND as string,
     SUPER_ADMIN_EMAIL:process.env.SUPER_ADMIN_EMAIL as string,
     SUPER_ADMIN_PASS:process.env.SUPER_ADMIN_PASS as string,
+    GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET as string,
+    GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID as string,
+    GOOGLE_CALLBACK_URL:process.env.GOOGLE_CALLBACK_URL as string,
+    FRONTEND_URL:process.env.FRONTEND_URL as string,
 }
 }
 
